4f315f5f2c8bc12cb28030f4359f82f2ad3f9362
Nils Schlüter <github@nilsschlueter.de>
2018-03-04 22:09:52 +0100

    - Split settings into dev and prod - Added JS/CSS compression

---
+++ backend/settings/dev.py
@@ -0,0 +1,28 @@
+from .common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# EMAIL BACKEND SERVER FOR TESTING
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# COMPRESSOR
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+COMPRESS_ROOT = os.path.join(BASE_DIR, "static/compressed")
+COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.CSSMinFilter']
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
