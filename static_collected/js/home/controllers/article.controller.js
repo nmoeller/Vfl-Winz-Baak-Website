91dd944d3111eacf3f410ae018dc994c8aca0a28
Nico Moeller <sternenstaub123>
2018-04-02 17:49:55 +0200

    seo adjustments

--- static_collected/js/home/controllers/article.controller.js
+++ static_collected/js/home/controllers/article.controller.js
@@ -5,20 +5,30 @@
         .module('website.article.controllers')
         .controller('ArticleController', ArticleController);
 
-    ArticleController.$inject = ['$scope', '$http', '$routeParams', '$sce'];
+    ArticleController.$inject = ['$scope', '$http', '$routeParams', '$sce', '$rootScope', '$location'];
 
-    function ArticleController($scope, $http, $routeParams, $sce) {
+    function ArticleController($scope, $http, $routeParams, $sce, $rootScope, $location) {
 
         var self = this;
         $scope.mainMenu = false;
-        var articleId = $routeParams.id ;
+        //console.log()
+        $rootScope.canonical = $location.absUrl()
 
-        $http.get("/articles/id/"+articleId)
+        function htmlToPlaintext(text) {
+            return text ? String(text).replace(/<[^>]+>/gm, '') : '';
+        }
+
+        var articleId = $routeParams.id;
+
+        $http.get("/articles/id/" + articleId)
             .then(function (response) {
                 $scope.articles = response.data[0];
                 
+
                 $scope.article = $scope.articles;
+            $rootScope.title = "Vfl Winz Baak - " + $scope.article.title
                 $scope.safeContent = $sce.trustAsHtml($scope.article.content)
+                $rootScope.description = htmlToPlaintext($scope.article.content)
             });
 
 
